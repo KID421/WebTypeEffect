4e48e8839dc1e6f3746eb15f7c50b416238e0656
KID421 <suchild7@gmail.com>
2019-08-03 16:57:35 +0800

    閃爍動畫與 js 變數

---
+++ typeeffect.js
@@ -0,0 +1,4 @@
+var texts = ["網頁設計師", "平面設計師", "遊戲開發者"];    // 打字 - 陣列
+var speed = 300;                                        // 打字速度 - 毫秒
+var index = 0;                                          // 索引值 - 從 0 開始
+var classname = ".type";                                // 類別名稱
